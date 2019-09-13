80312af3a573e7b9de2604d9e8c54046ef8ab440
marco-vannoli95 <55249248+marco-vannoli95@users.noreply.github.com>
2019-09-13 11:18:41 +0200

    Update game.js

--- game.js
+++ game.js
@@ -235,7 +235,7 @@ class DiMaio {
       this.x = random(0,window.innerWidth -80);
       this.y = random(0,150);
       this.diameter = 60
-      this.speed = 2;
+      this.speed = 2.5;
     }
   
     move() {
